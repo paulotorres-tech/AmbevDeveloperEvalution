7246ca21b1be48864938d59e7fee12c2802eb49c
Paulo Torres <nery.torres@outlook.com>
2025-04-16 15:44:52 -0300

    feat: define CreateSaleCommand and CreateSaleResult DTOs

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
+{
+    public class CreateSaleCommand : IRequest<CreateSaleResult>
+    {
+        public string SaleNumber { get; set; } = string.Empty;
+        public DateTime SaleDate { get; set; }
+
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+
+        public int BranchId { get; set; }
+        public string BranchName { get; set; } = string.Empty;
+
+        public List<SaleItemDto> Items { get; set; } = new();
+    }
+
+    public class SaleItemDto
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
