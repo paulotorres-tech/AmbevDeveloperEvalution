a8ec23945461e1430548ab4ce9bd1b4ef5d55c44
Paulo Torres <nery.torres@outlook.com>
2025-04-16 15:45:36 -0300

    chore: add AutoMapper profile for CreateSale

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
+{
+    public class CreateSaleProfile : Profile
+    {
+        public CreateSaleProfile() {
+            CreateMap<CreateSaleCommand, Sale>();
+
+            CreateMap<Sale, CreateSaleCommand>();
+        }
+    }
+}
