11f03f23693b46016402261306ce4cc8681009cd
Paulo Torres <nery.torres@outlook.com>
2025-04-16 16:50:15 -0300

    feat: implement CancelSale command and handler with repository integration

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CancelSale/CancelSaleResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CancelSale
+{
+    public class CancelSaleResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
