7941fa1e3e9b52c55a432b2c6be4ca900294eb9e
Paulo Torres <nery.torres@outlook.com>
2025-04-16 16:53:02 -0300

    feat: implement GetSaleBySaleNumber command and handler with repository integration

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/GetSaleByNumber/GetSaleByNumberQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.GetSaleByNumber
+{
+    public class GetSaleByNumberQuery : IRequest<GetSaleByNumberResult>
+    {
+        public string SaleNumber { get; set; } = string.Empty;
+    }
+}
