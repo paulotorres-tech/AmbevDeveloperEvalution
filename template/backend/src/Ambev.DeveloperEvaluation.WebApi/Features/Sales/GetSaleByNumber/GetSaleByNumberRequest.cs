4a981393de545d6a5858017b3067db679a5f8630
Paulo Torres <nery.torres@outlook.com>
2025-04-16 17:56:43 -0300

    feat: implement GetSaleByNumber using GET /sales/{saleNumber}

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/GetSaleByNumber/GetSaleByNumberRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSaleByNumber
+{
+    /// <summary>
+    /// Request to get sale details by sale number.
+    /// </summary>
+    public class GetSaleByNumberRequest : IRequest<GetSaleByNumberResponse>
+    {
+        public string SaleNumber { get; set; } = string.Empty;
+    }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/GetSaleByNumberRequestProfile.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.GetSaleByNumber;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSaleByNumber;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings
+{
+    public class GetSaleRequestProfile : Profile
+    {
+        public GetSaleRequestProfile()
+        {
+            CreateMap<GetSaleByNumberRequest, GetSaleByNumberQuery>();
+
+            CreateMap<GetSaleByNumberResult, GetSaleByNumberResponse>();
+            CreateMap<SaleItemResult, GetSaleItemResponse>();
+        }
+    }
+}
