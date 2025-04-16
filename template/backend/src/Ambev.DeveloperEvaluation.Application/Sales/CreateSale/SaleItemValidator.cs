ccfcf746744da7a49c2534f77eb9256b881446a6
Paulo Torres <nery.torres@outlook.com>
2025-04-16 15:51:49 -0300

    feat: implement CreateSale command, validator and handler

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/SaleItemValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
+{
+    public  class SaleItemValidator : AbstractValidator<SaleItemDto>
+    {
+        public SaleItemValidator() {
+            RuleFor(item => item.ProductId)
+                .GreaterThan(0);
+
+            RuleFor(item => item.Quantity)
+                .GreaterThan(0)
+                .LessThanOrEqualTo(20);
+
+            RuleFor(item => item.UnitPrice)
+                .GreaterThan(0);
+        }
+    }
+}
